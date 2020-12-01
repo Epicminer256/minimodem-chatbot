d4b7fa1267aa9e5378928e76d47a6e3a46ac3c51
IanKing <72170170+Epicminer256@users.noreply.github.com>
2020-11-30 19:11:22 -0600

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -20,30 +20,30 @@ read ratespeed
 
 clear
 while true; do
-    test=$(minimodem --quiet --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --rx --rx-one $ratespeed)
+    test=$(minimodem --quiet --sync-byte=0xAB --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --rx --rx-one $ratespeed)
     echo Heard something
 	if [ "$test" = "$command1" ]; then
         sleep 0.5
         echo Sending $test message...
-        $command1act | minimodem --quiet --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
+        $command1act | minimodem --quiet --sync-byte=0xAB --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
     fi
     
 	if [ "$test" = "$command2" ]; then
         sleep 0.5
         echo Sending $test message...
-        $command2act | minimodem --quiet --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
+        $command2act | minimodem --quiet --sync-byte=0xAB --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
     fi
     
 	if [ "$test" = "$command3" ]; then
         sleep 0.5
         echo Sending $test message...
-        $command3act | minimodem --quiet --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
+        $command3act | minimodem --quiet --sync-byte=0xAB --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
     fi
     
 	if [ "$test" = "$command4" ]; then
         sleep 0.5
         echo Sending $test message...
-        $command4act | minimodem --quiet --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
+        $command4act | minimodem --quiet --sync-byte=0xAB --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --tx $ratespeed
     fi
     sleep 0.1
 done
